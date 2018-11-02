71162e70e500104d7aa918a8bcb81554e1ba8900
nathaliegriffin <44684136+nathaliegriffin@users.noreply.github.com>
2018-11-02 10:42:17 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,346 @@
+
+
+
+$(document).on('keypress', function(i){
+  if (i.key === ',') {
+    $('.type').addClass('pink');
+  } else {
+    $('.type').removeClass('pink');
+  }
+});
+
+
+$(document).on('keypress', function(i){
+  if (i.key === 'a') {
+    $('.A').addClass('yellow');
+  } else {
+    $('.A').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'b') {
+    $('.B').addClass('green');
+  } else {
+    $('.B').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'c') {
+    $('.C').addClass('red');
+  } else {
+    $('.C').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'd') {
+    $('.D').addClass('pink');
+  } else {
+    $('.D').removeClass('pink');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'e') {
+    $('.E').addClass('purple');
+  } else {
+    $('.E').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'f') {
+    $('.F').addClass('yellow');
+  } else {
+    $('.F').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'g') {
+    $('.G').addClass('red');
+  } else {
+    $('.G').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'h') {
+    $('.H').addClass('orange');
+  } else {
+    $('.H').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'i') {
+    $('.I').addClass('green');
+  } else {
+    $('.I').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'j') {
+    $('.J').addClass('yellow');
+  } else {
+    $('.J').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'k') {
+    $('.K').addClass('red');
+  } else {
+    $('.K').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'l') {
+    $('.L').addClass('purple');
+  } else {
+    $('.L').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'm') {
+    $('.M').addClass('orange');
+  } else {
+    $('.M').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'n') {
+    $('.N').addClass('green');
+  } else {
+    $('.N').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'o') {
+    $('.O').addClass ('yellow');
+  } else {
+    $('.O').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'p') {
+    $('.P').addClass('purple');
+  } else {
+    $('.P').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'q') {
+    $('.Q').addClass('orange');
+  } else {
+    $('.Q').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'r') {
+    $('.R').addClass('orange');
+  } else {
+    $('.R').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 's') {
+    $('.S').addClass('green');
+  } else {
+    $('.S').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 't') {
+    $('.T').addClass('pink');
+  } else {
+    $('.T').removeClass('pink');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'u') {
+    $('.U').addClass('purple');
+  } else {
+    $('.U').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'v') {
+    $('.V').addClass('green');
+  } else {
+    $('.V').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'w') {
+    $('.W').addClass('red');
+  } else {
+    $('.W').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'x') {
+    $('.X').addClass('yellow');
+  } else {
+    $('.X').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'y') {
+    $('.Y').addClass('green');
+  } else {
+    $('.Y').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === 'z') {
+    $('.Z').addClass('red');
+  } else {
+    $('.Z').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '0') {
+    $('.a0').addClass('green');
+  } else {
+    $('.a0').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '1') {
+    $('.a1').addClass('red');
+  } else {
+    $('.a1').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '2') {
+    $('.a2').addClass('orange');
+  } else {
+    $('.a2').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '3') {
+    $('.a3').addClass('purple');
+  } else {
+    $('.a3').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '4') {
+    $('.a4').addClass('yellow');
+  } else {
+    $('.a4').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '5') {
+    $('.a5').addClass('pink');
+  } else {
+    $('.a5').removeClass('pink');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '6') {
+    $('.a6').addClass('purple');
+  } else {
+    $('.a6').removeClass('purple');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '7') {
+    $('.a7').addClass('yellow');
+  } else {
+    $('.a7').removeClass('yellow');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '7') {
+    $('.a7').addClass('green');
+  } else {
+    $('.a7').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '8') {
+    $('.a8').addClass('red');
+  } else {
+    $('.a8').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '9') {
+    $('.a9').addClass('orange');
+  } else {
+    $('.a9').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '/') {
+    $('.qst').addClass('orange');
+  } else {
+    $('.qst').removeClass('orange');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === '.') {
+    $('.exl').addClass('red');
+  } else {
+    $('.exl').removeClass('red');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === ',') {
+    $('.and').addClass('green');
+  } else {
+    $('.and').removeClass('green');
+  }
+});
+
+$(document).on('keypress', function(i){
+  if (i.key === ';') {
+    $('.per').addClass('orange');
+  } else {
+    $('.per').removeClass('orange');
+  }
+});
+
+
+
+
+
+
+
